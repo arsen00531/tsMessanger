a22bc1cb9a1fd5d7eb2b34a1a3a728e0ac05ca8c
Арсен <arsendag505@gmail.com>
2023-11-23 01:37:58 +0300

    commit

--- src/modules/soket.ts
+++ src/modules/soket.ts
@@ -27,14 +27,14 @@ const socket = (io: Server): void => {
             const {name, text}: {name: string, text: string} = data
             io.sockets.emit('add mess', {text, name});
             
-            await db.query(`INSERT INTO chat (name, text) VALUES ($1, $2)`, [name, text]);
+            db.query(`INSERT INTO chat (name, text) VALUES ($1, $2)`, [name, text]);
         });
 
         // anonimeChat
         socket.on('send', async (data): Promise<void> => {
             const {name, guest, text}: {name: string, guest: string, text: string} = data
 
-            await db.query(`INSERT INTO anonimeChat (text, name, guest) VALUES ($1, $2, $3)`, [text, name, guest] )
+            db.query(`INSERT INTO anonimeChat (text, name, guest) VALUES ($1, $2, $3)`, [text, name, guest] )
 
             io.sockets.emit('give', {text, name})
         })
